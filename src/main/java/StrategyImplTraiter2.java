fe22d587ab7f698c09dc15fbdca0c93230e74754
hajar tajayouti <tajayoutihajar@gmail.com>
2022-10-28 12:01:11 +0100

    modif

---
+++ src/main/java/StrategyImplTraiter2.java
@@ -0,0 +1,7 @@
+public class StrategyImplTraiter2 implements Strategy {
+    @Override
+    public void operationStrategy() {
+
+        System.out.println("Traiter  hajar TAJAYOUTI VERSION 2 ");
+    }
+}
